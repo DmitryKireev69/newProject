2f4740e589c5d9ef1c03c9f96df8c2c5d94dfcd4
kireev <kireev@rbt.ru>
2022-11-16 14:34:57 +0300

    Добалвение элементов на страницу

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import React, {useState} from 'react';
+import {Product} from './components/Product';
+import {products} from './data/products';
 
 function App() {
-
-const [count, setCount] = useState(0);
-
   return (
-      <div>
-        <h1>Heddll</h1>
-        <button className="border 2px solid" onClick={ () => setCount(count + 1)}>Click Me!</button>
-        <h1>{count}</h1>
+      <div className="container mx-auto max-w-2xl pt-5">
+          <Product product={products[0]}/>
+          <Product product={products[1]}/>
       </div>
-
   );
 }
 
